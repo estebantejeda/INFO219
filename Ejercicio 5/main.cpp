8d9d5562e71dd04a031b9ae81abcc9af3d30054a
Esteban Tejeda <esteban.tejeda@alumnos.uach.cl>
2020-08-02 15:46:34 -0400

    Ejercicio 5 terminado

--- Ejercicio 5/main.cpp
+++ Ejercicio 5/main.cpp
@@ -8,6 +8,7 @@ using namespace std;
 int enterInput();
 void initArray(int **Array, int x, int y);
 void randomArray(int **Array, int x, int y);
+void pathArray(int **Array, int x, int y);
 void printArray(int **Array, int x, int y);
 
 int main(){
@@ -16,6 +17,7 @@ int main(){
     initArray(M,m,m);
     randomArray(M,m,m);
     printArray(M,m,m);
+    pathArray(M,m,m);
     return 0;
 }
 
@@ -37,6 +39,28 @@ void randomArray(int **Array, int x, int y){
     for(int i = 0; i < x; i++) {
         for(int j = 0; j < y; j++) Array[i][j] = rand()%2;
     }
+    Array[0][0] = 1;
+    Array[x-1][y-1] = 1;
+}
+
+void pathArray(int **Array, int x, int y){
+    int **A = new int*[x];
+    initArray(A,x,y);
+    for(int i = 0; i < x; i++) {
+        for(int j = 0; j < y; j++){
+            if(Array[i][j] == 1){
+                int U, V;
+                if (j-1 < 0) U = 0;
+                else U = A[i][j-1];
+                if (i-1 < 0) V = 0;
+                else V = A[i-1][j];
+                A[i][j] = U+V;
+                if (A[0][0] != 1) A[0][0] = 1;
+            }
+            else A[i][j] = 0;
+        }
+    }
+    cout << A[x-1][y-1] << endl;
 }
 
 void printArray(int **Array, int x, int y){
